04bf232c2a2446980e99251aab047a7ba41ec171
Daniel Cavalcante <danielcdesouza@gmail.com>
2019-03-16 10:48:22 -0300

    Add new files

---
+++ app/home/home.controller.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+    
+    angular.module('baas-front-end')
+    .controller('HomeCtrl', HomeCtrl);
+  
+    HomeCtrl.$inject = [];
+  
+    /* @ngInject */
+    function HomeCtrl() {
+      var vm = this;
+
+      activate();
+
+      function activate() {
+          
+      }
+  
+    }
+  })();
+  
